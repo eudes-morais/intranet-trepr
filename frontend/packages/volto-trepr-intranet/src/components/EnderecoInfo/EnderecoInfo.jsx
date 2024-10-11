d4331c404f6b6155db7a6f864d60c27e757f2009
eudes-morais <eudes.morais@gmail.com>
2024-10-11 16:13:13 -0300

    Exercício 27: Campos endereço para tipo Área

---
+++ frontend/packages/volto-trepr-intranet/src/components/EnderecoInfo/EnderecoInfo.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container } from '@plone/components';
+
+const EnderecoInfo = ({ content }) => {
+  const { endereco, complemento, cidade, estado, cep } = content;
+
+  return (
+    <Container narrow className="eendereco">
+      <Container className="endereco">
+        <span>Endereço</span>: <span>{endereco}</span>
+      </Container>
+      <Container className="complemento">
+        <span>Complemento</span>: <span>{complemento}</span>
+      </Container>
+      <Container className="cidade">
+        <span>Cidade</span>: <span>{cidade}</span>
+      </Container>
+      <Container className="estado">
+        <span>Estado</span>: <span>{estado}</span>
+      </Container>
+      <Container className="cep">
+        <span>CEP</span>: <span>{cep}</span>
+      </Container>
+    </Container>
+  );
+};
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+EnderecoInfo.propTypes = {
+  content: PropTypes.shape({
+    endereco: PropTypes.string,
+    complemento: PropTypes.string,
+    cidade: PropTypes.string,
+    estado: PropTypes.string,
+    cep: PropTypes.string,
+  }).isRequired,
+};
+
+export default EnderecoInfo;
