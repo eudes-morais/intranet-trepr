796c72f93c5d50c19a771c5c62d04a0a37603a4a
eudes-morais <eudes.morais@gmail.com>
2024-10-11 14:03:22 -0300

    Ex 23: Visão para tipo Área

--- frontend/packages/volto-trepr-intranet/src/index.js
+++ frontend/packages/volto-trepr-intranet/src/index.js
@@ -1,3 +1,6 @@
+// Views
+import AreaView from './components/Views/AreaView';
+
 const applyConfig = (config) => {
   config.settings = {
     ...config.settings,
@@ -19,6 +22,15 @@ const applyConfig = (config) => {
       },
     ],
   };
+
+  // Views
+  config.views.contentTypesViews = {
+    ...config.views.contentTypesViews,
+    Area: AreaView,
+  };
+
+  // Blocos
+  /// Altera bloco mapLibreBlock
   config.blocks.blocksConfig['mapLibreBlock']['tileLayers'] = [
     {
       id: 'osm',
