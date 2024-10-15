18ca7150df882e89b8acd4cbca39bd1ebe9a205d
eudes-morais <eudes.morais@gmail.com>
2024-10-15 16:45:36 -0300

    Ex 39: Sobreescrever a visão de Grade

--- frontend/packages/volto-trepr-intranet/src/index.js
+++ frontend/packages/volto-trepr-intranet/src/index.js
@@ -1,6 +1,8 @@
 // Views
 import AreaView from './components/Views/AreaView';
 import PessoaView from './components/Views/PessoaView';
+// Blocks
+import AreaGridItem from './components/Blocks/Grid/AreaGridItem';
 
 const applyConfig = (config) => {
   config.settings = {
@@ -30,6 +32,14 @@ const applyConfig = (config) => {
     Area: AreaView,
     Pessoa: PessoaView,
   };
+
+  // Blocos
+  /// Grid
+  config.registerComponent({
+    name: 'GridListingItemTemplate',
+    component: AreaGridItem,
+    dependencies: 'Area',
+  });
 
   // Blocos
   /// Altera bloco mapLibreBlock
